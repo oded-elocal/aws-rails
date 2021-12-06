979bc4d49de32eb580911c015721a4254b1b2e56
Oded Alter <oded.alter@elocal.com>
2021-12-05 23:41:11 -0500

    Users routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+  # THIS IS INSTEAD OF THE 3 ABOVE
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+
   resources :videos
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
